efc45272fe4b39559c58219b465935be88023018
panchuz <139817605+panchuz@users.noreply.github.com>
2023-07-27 17:07:22 -0300

    ${encabezado}
    sin comilla simple

--- linux_config_inicial.sh
+++ linux_config_inicial.sh
@@ -79,10 +79,7 @@ function main () {
 	local script_directorio_nombre="$(script_directorio_nombre_stdout)"
 	printf  "script_directorio_nombre:\t%s\n" "${script_directorio_nombre}"
 	local encabezado="$(generacion_encabezado_stdout)"
-	printf  "encabezado:\t\t\n%s\n" '${encabezado}'
-	echo "último comando"
- 	echo ${BASH_SOURCE}
- 	printf "main se está ejecutando. seguimos...\n"
+	printf  "encabezado:\t\t\n%s\n" ${encabezado}
 }
 
 main
