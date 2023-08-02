144ed02cbf58114d4cd044f04f04bb42d31178e4
panchuz <139817605+panchuz@users.noreply.github.com>
2023-08-02 16:38:22 -0300

    debian_dist-upgrade
    2 sabores:
    - nuevo_debian_dist-upgrade
    - debian_dist-upgrade_reboot

--- generales.func.sh
+++ generales.func.sh
@@ -1,7 +1,10 @@
-# determinación del directorio de este script para que conste en el encabezado
+# FUNCIONES GENERALES para bash scripts
+
+# Determinación robusta* del directorio de este script para que conste en el encabezado
+# *a prueba de source y simlinks de directorio y archivo
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 function script_directorio_nombre_stdout () {
-  local source=${BASH_SOURCE[0]}
+	local source=${BASH_SOURCE[0]}
 	while [ -L "$source" ]; do # resolve $source until the file is no longer a symlink
 	  local directorio=$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )
 	  source=$(readlink "$source")
@@ -11,3 +14,38 @@ function script_directorio_nombre_stdout () {
 	local nombre=$(basename "$source")
 	printf "${directorio}/${nombre}"
 }
+
+
+# apt-get update and upgrade automate and unattended
+# https://www.cyberciti.biz/faq/explain-debian_frontend-apt-get-variable-for-ubuntu-debian/
+function nuevo_debian_dist-upgrade () {
+	# local NEEDRESTART_MODE=a
+	local DEBIAN_FRONTEND=noninteractive
+	## Questions that you really, really need to see (or else). ##
+	local DEBIAN_PRIORITY=critical
+	apt-get -qy update
+	apt-get -qy -o "Dpkg::Options::=--force-confnew" dist-upgrade
+	apt-get -qy clean
+	apt-get -qy autoremove
+}
+
+
+# apt-get update and upgrade automate and unattended
+# https://www.cyberciti.biz/faq/explain-debian_frontend-apt-get-variable-for-ubuntu-debian/
+function debian_dist-upgrade_reboot () {
+	# local NEEDRESTART_MODE=a
+	local DEBIAN_FRONTEND=noninteractive
+	## Questions that you really, really need to see (or else). ##
+	local DEBIAN_PRIORITY=critical
+	apt-get -qy update
+	apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
+	apt-get -qy clean
+	apt-get -qy autoremove
+	if [ -f /var/run/reboot-required ]; then
+		reboot
+	fi
+}
+
+ # PROXIMAMENTE
+ # Verificación de privilegios
+ # ...
