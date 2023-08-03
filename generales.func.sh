a914421b6d835e580627d44cd425436392bf591a
panchuz <139817605+panchuz@users.noreply.github.com>
2023-08-03 17:07:29 -0300

    printf en lugar de echo

--- generales.func.sh
+++ generales.func.sh
@@ -50,7 +50,7 @@ function debian_dist-upgrade_reboot () {
 # https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
 function verif_privilegios_root_exitcode () {
 	if (( $EUID != 0 )); then
-		echo "ERROR: Este script se debe correr con privilegios root"
+		printf "ERROR: Este script se debe correr con privilegios root\n"
 		exit 1
   	else
    		exit 0
