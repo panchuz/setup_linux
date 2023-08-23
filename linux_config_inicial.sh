068d2cd84c8768768b07e71bae5171c0bbc31346
panchuz <139817605+panchuz@users.noreply.github.com>
2023-08-23 12:03:54 -0300

    printf "Se necesita rebootear\n"

--- linux_config_inicial.sh
+++ linux_config_inicial.sh
@@ -60,11 +60,16 @@ function main () {
 	printf "/var/run/reboot-required= "
 	cat /var/run/reboot-required
 	printf "\n"
+ 	if [ -f /var/run/reboot-required ]; then
+		printf "Se necesita rebootear\n"
+	else
+ 		printf "NO se necesita rebootar\n"
+ 	fi
 }
 
 # Verificación de privilegios
 # https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
-if (( $EUID = 0 )); then
+if (( $EUID == 0 )); then
 	main
 else
 	printf "ERROR: Este script se debe ejecutar con privilegios root\n"
