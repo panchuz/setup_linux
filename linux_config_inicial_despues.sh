b040b8b16f4aed173e02e5657a7c727d33afadf3
panchuz <139817605+panchuz@users.noreply.github.com>
2023-08-23 12:45:40 -0300

    Create linux_config_inicial_despues.sh

---
+++ linux_config_inicial_despues.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source <(wget --quiet -O - https://raw.githubusercontent.com/panchuz/linux_config_inicial/main/generales.func.sh)
+
+# creado por panchuz
+# para automatizar la segunda parte de la configuración inicial de lxc generado en base
+# al template debian-12-standard_12.0-1_amd64.tar.zst de Proxmox VE
+
+
+#------------------FUNCIÓN PRINCIPAL------------------
+function principal () {
+	# aca va el comentario
+	printf  "linux_config_inicial_despues.sh se está ejecutando:\n"
+}
+
+principal
