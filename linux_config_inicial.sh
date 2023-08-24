242d42f26e6d27cad073507c6f5ce50364890c05
panchuz <139817605+panchuz@users.noreply.github.com>
2023-08-24 17:04:26 -0300

    crear_archivo_reinicio-servicio

--- linux_config_inicial.sh
+++ linux_config_inicial.sh
@@ -12,6 +12,8 @@ export TZ='America/Argentina/Buenos_Aires'
 # el contenido de la sig variable sirve para appendear a los nombres de los archivos creados por este script
 MARCA="_panchuz"
 
+# resto de las variables se definen en función principal
+
 
 # GENERACIÓN DEL ENCABEZADO PARA LOS ARCHIVOS DE CONFIGURACIÓN
 function generacion_encabezado_stdout () {
@@ -43,13 +45,39 @@ function config_huso_horario () {
 	timedatectl set-timezone ${TZ}
 }
 
+
+
+# SERVICIO PARA EL REINICIO
+# https://wiki.debian.org/systemd#Creating_or_altering_services
+function crear_archivo_reinicio-servicio () {
+	cat >/etc/systemd/system/reinicio${MARCA}.servicio <<-EOF
+	${encabezado}
+	# https://wiki.debian.org/systemd#Creating_or_altering_services
+	# https://operavps.com/docs/run-command-after-boot-in-linux/
+ 
+	[Unit]
+	Description=Ejecuta /root/${script_reinicio} por única vez luego de reinicio
+	After=network.target auditd.service
+	ConditionFileIsExecutable=/root/${script_reinicio}
+
+ 	[Service]
+ 	Type=oneshot
+	ExecStart=/bin/bash /root/${script_reinicio}
+ 	# desactiva el servicio luego que cumplió su función:
+ 	ExecStartPost=/bin/systemctl disable reinicio${MARCA}
+
+	[Install]
+	WantedBy=multi-user.target
+EOF
+}
+
 #------------------FUNCIÓN PRINCIPAL------------------
 function principal () {
  	
   	# script para seguir el proceso luego del reboot (o del no reboot)
-	local script_despues=linux_config_inicial_despues.sh
-	wget -qP /root https://github.com/panchuz/linux_config_inicial/raw/main/${script_despues} &&
- 		chmod +x /root/${script_despues}
+	export script_reinicio=linux_config_inicial_reinicio.sh
+	wget -qP /root https://github.com/panchuz/linux_config_inicial/raw/main/${script_reinicio} &&
+ 		chmod +x /root/${script_reinicio}
    
         # genera y guarda encabezado de texto para uso posterior en archivos creados por el script
  	local encabezado="$(generacion_encabezado_stdout)"
@@ -68,7 +96,7 @@ function principal () {
 		printf "Se necesita rebootear\n"
  	else
  		printf "NO se necesita rebootar\n"
-   		/root/${script_despues}
+   		/root/${script_reinicio}
   	fi
 }
 
