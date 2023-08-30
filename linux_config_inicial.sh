ee36e24a577d19dff4868cfe8e554ba8574337e4
panchuz <139817605+panchuz@users.noreply.github.com>
2023-08-30 16:37:25 -0300

    Unattended-Upgrade::MailReport "on-change";

--- linux_config_inicial.sh
+++ linux_config_inicial.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-source <(wget --quiet -O - https://raw.githubusercontent.com/panchuz/linux_config_inicial/main/generales.func.sh)
+# $1: contraseña de aplicación para Gmail 
 
-# creado por panchuz
-# para automatizar la configuración inicial de lxc generado en base
-# al template debian-12-standard_12.0-1_amd64.tar.zst de Proxmox VE
+#######################################################################
+#  creado por panchuz                                                 #
+#  para automatizar la configuración inicial de lxc generado en base  #
+#  al template debian-12-standard_12.0-1_amd64.tar.zst de Proxmox VE  #
+#######################################################################
+
+source <(wget --quiet -O - https://raw.githubusercontent.com/panchuz/linux_config_inicial/main/generales.func.sh)
 
 # Opciones para la configuración
 export LANG=C.utf8 # quedará de forma permamente. Ver: crear_archivo_profile_local ()
@@ -50,40 +54,62 @@ config_huso_horario () {
 # https://www.lynksthings.com/posts/sysadmin/mailserver-postfix-gmail-relay/
 # https://forum.proxmox.com/threads/get-postfix-to-send-notifications-email-externally.59940/
 # https://serverfault.com/questions/744761/postfix-aliases-will-be-ignored
+# https://www.computernetworkingnotes.com/linux-tutorials/how-to-configure-a-postfix-null-client-step-by-step.html
 config_postfix_nullclient_gmail () {
-	cp /etc/postfix/main.cf /etc/postfix/main.cf.ORIGINAL${MARCA}
-	# configuración postfix >> /etc/postfix/main.cf
-	postconf 'mydestination =' \
-		'relayhost = [smtp.gmail.com]:587' \
-		'inet_interfaces = loopback-only' \
-		'compatibility_level = 3.6' \
-		'smtp_sasl_security_options = noanonymous' \
-		'smtp_tls_security_level = encrypt' \
-		'smtp_sasl_auth_enable = yes' \
-		'smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt' \
-		'smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd'
+# $1: contraseña de aplicación para Gmail
+
+	# el archivo sig. guarda las credenciales para usar el SMTP server de Gmail
 	cat >/etc/postfix/sasl/sasl_passwd <<-EOF
 		${encabezado}
 		# https://www.lynksthings.com/posts/sysadmin/mailserver-postfix-gmail-relay/
 		#
-		[smtp.gmail.com]:587 panchuz.ar+pampa@gmail.com:${1}
+		[smtp.gmail.com]:587 panchuz.ar@gmail.com:${1}
 	EOF
 	postmap /etc/postfix/sasl/sasl_passwd
 	chmod 0600 /etc/postfix/sasl/sasl_passwd*
+
+	# mapea usuarios locales a direcciones de mail
+	cat >/etc/postfix/generic <<-EOF
+		${encabezado}
+		# https://www.postfix.org/STANDARD_CONFIGURATION_README.html#fantasy
+		#
+		root panchuz.ar+$(hostname)@gmail.com
+	EOF
+	postmap /etc/postfix/generic
+	
+	# configuración postfix >> /etc/postfix/main.cf
+	cp /etc/postfix/main.cf /etc/postfix/main.cf.ORIGINAL${MARCA}
+	postconf 'mydestination =' \
+		'relayhost = [smtp.gmail.com]:587' \
+		'inet_interfaces = loopback-only' \
+		'compatibility_level = 3.6' \
+		'smtp_tls_security_level = encrypt' \
+		'smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt' \
+		'smtp_sasl_security_options = noanonymous' \
+		'smtp_sasl_auth_enable = yes' \
+		'smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd' \
+		'smtp_generic_maps = hash:/etc/postfix/generic'
 	postfix reload
-	#systemctl restart postfix
 }
 
 
-# --- CONFIGURACIÓN unattended-upgrades PARA PRUEBA MAIL ---
-# ${encabezado//\#///} subtituye "#" por "//". Ref: https://stackoverflow.com/a/43421455
+# --- CONFIGURACIÓN unattended-upgrades PRUEBA MAIL ---
 config_unattended-upgrades_prueba_mail () {
+
+	cat >/etc/apt/apt.conf.d/51unattended-upgrades${MARCA} <<-EOF
+		Unattended-Upgrade::Mail "root";
+		Unattended-Upgrade::MailReport "always"; /* SOLO PARA PROBAR */
+	EOF
+
+	unattended-upgrade -d && printf "Checkear recepción de mail de unattended-upgrades"
+
+	# ${encabezado//\#///} subtituye "#" por "//". Ref: https://stackoverflow.com/a/43421455
 	cat >/etc/apt/apt.conf.d/51unattended-upgrades${MARCA} <<-EOF
 		${encabezado//\#///}
 		// https://wiki.debian.org/UnattendedUpgrades#Unattended_Upgrades
 		// 
-	
 		Unattended-Upgrade::Mail "root";
+		Unattended-Upgrade::MailReport "on-change";
 	EOF
 }
 
@@ -121,7 +147,7 @@ crear_reinicio-service () {
 
 #------------------FUNCIÓN PRINCIPAL------------------
 principal () {
- 	
+# $1: contraseña de aplicación para Gmail
   	# script para seguir el proceso luego del reboot (o del no reboot)
    	# debe coincidir con el de https://github.com/panchuz/linux_config_inicial/raw/main/....sh
 	local script_reinicio=linux_config_inicial_reinicio.sh
@@ -149,13 +175,12 @@ principal () {
 	# mailx es pedido en /etc/apt/apt.conf.d/50unattended-upgrades para notificar por mail
 	# apt-listchanges es indicado en https://wiki.debian.org/UnattendedUpgrades#Automatic_call_via_.2Fetc.2Fapt.2Fapt.conf.d.2F20auto-upgrades
 	debian_dist-upgrade_install libsasl2-modules postfix-pcre bsd-mailx apt-listchanges unattended-upgrades sudo
+	##### rsyslog: https://itslinuxfoss.com/find-postfix-log-files/
 
-
-	# configurar postfix:
-	config_postfix_nullclient_gmail
+	# configurar postfix como nullclient/smtp de gmail/no-FQDN:
+	config_postfix_nullclient_gmail $1
  
  	# configurar uanttended-upgrades
-	# escribir $(encabezado) Unattended-Upgrade::Mail "root"; > 51unattended-upgrades${MARCA}
 	config_unattended-upgrades_prueba_mail
 
 	# mail de prueba: unattended-upgrade -d
@@ -181,7 +206,7 @@ principal () {
 # Verificación de privilegios
 # https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
 if (( $EUID == 0 )); then
-	principal
+	principal $1
 else
 	printf "ERROR: Este script se debe ejecutar con privilegios root\n"
 fi
