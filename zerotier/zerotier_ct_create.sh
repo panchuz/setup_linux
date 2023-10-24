184fc0d2095d5a15e47bb40bdf9295e819967f95
panchuz <139817605+panchuz@users.noreply.github.com>
2023-10-24 17:41:36 -0300

    --rootfs volume=$container_storage,mountoptions=nodatacow;autodefrag;noatime;lazytime,size=2 \

--- zerotier/zerotier_ct_create.sh
+++ zerotier/zerotier_ct_create.sh
@@ -7,6 +7,10 @@ container_id="$1" # passwd para desencriptar link_args.aes256
 #  desde la consola de Proxmox VE  
 #######################################################################
 
+# testing configuration for peludo
+template_storage=cola
+container_storage=cola
+
 # verificación de la cantidad de argumentos
 if [ $# -ne 1 ]; then
     echo "Uso: ${BASH_SOURCE[0]} container_id"
@@ -17,18 +21,23 @@ fi
 #source <(wget --quiet -O - https://raw.githubusercontent.com/panchuz/linux_config_inicial/main/generales.func.sh)
 
 
-pct create $contairner_id local:vztmpl/debian-10-standard_10.7-1_amd64.tar.gz??? \
-	-hostname zerotier \
-	-description "Zerotier with NAT-Masq access to phisical net" \
-	-cores 1 \
-	-memory 512 \
-	-swap 512 \
-	-net0 name=eth0,bridge=vmbr0,firewall=1,ip=dhcp,type=veth \
-	-storage local-lvm ????? \
-	-unprivileged 1 \
-	-timezone host \
-	-onboot 1 \
-	#-hookscript <string>
+pct create $contairner_id $template_storage:vztmpl/debian-12-standard_12.0-1_amd64.tar.gz \
+	--ostype debian \
+	--tags deb12 zerotier \
+	--password pancho00 \
+	--protction 1 \
+	--hostname zerotier \
+	--description "Zerotier with NAT-Masq access to phisical net" \
+	--cores 1 \
+	--memory 512 \
+	--swap 512 \
+	--net0 name=eth0,bridge=vmbr0,firewall=1,ip=dhcp,type=veth \
+	--storage $container_storage \
+	--rootfs volume=$container_storage,mountoptions=nodatacow;autodefrag;noatime;lazytime,size=2 \
+	--unprivileged 1 \
+	--timezone host \
+	--onboot 1 #\
+	#--hookscript <string> Script that will be exectued during various steps in the containers lifetime.
 	#|| return 1
 
 	# the two following lines must be written to .conf file directly
